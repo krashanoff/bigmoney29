c368c7dc2bae8346b8d9f4c459170e21ec336450
krashanoff <leo@krashanoff.com>
2022-09-19 00:38:20 -0700

    Persist storage.

--- ui/src/features/index.js
+++ ui/src/features/index.js
@@ -3,9 +3,34 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth";
 import assnReducer from "./assignments";
 
-export default configureStore({
+const loadState = () => {
+  try {
+    const state = localStorage.getItem("state");
+    if (!state) {
+      return undefined;
+    }
+    return JSON.parse(state);
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  localStorage.setItem("state", JSON.stringify(state));
+};
+
+const persistedState = loadState();
+
+const state = configureStore({
   reducer: {
     auth: authReducer,
     assignments: assnReducer,
   },
+  preloadedState: persistedState,
 });
+
+state.subscribe(() => {
+  saveState(state.getState());
+});
+
+export default state;
